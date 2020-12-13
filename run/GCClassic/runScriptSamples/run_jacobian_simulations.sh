2dabc874387283268633af13ba4f8f31d1dea2c3
Melissa Sulprizio <mpayer@seas.harvard.edu>
2020-12-13 16:02:37 -0500

    Update sample run scripts for CH4 simulations
    (1) Rename run.template to ch4_run.template to indicate it is setup for
        a CH4 simulation. The only special line for this simulation is in the
        cleanup section where it deletes the HEMCO restart.
    
    (2) Replace run_array_job and submit_array_jobs with scripts from Daniel
        Varon:
         - run_jacobian_simulations.sh is set up to run four jobs at a time to
           avoid submitting many short simulations.
         - submit_jacobian_simulations_array.sh now uses -W flag to tell
           scheduler to wait for all jobs to finish.
    
    Signed-off-by: Melissa Sulprizio <mpayer@seas.harvard.edu>

---
+++ run/GCClassic/runScriptSamples/run_jacobian_simulations.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#SBATCH -J {RunName}
+#SBATCH -c 1
+#SBATCH -N 1
+#SBATCH -p huce_intel
+#SBATCH --mem 2000
+#SBATCH -t 0-2:00
+
+### Run directory
+RUNDIR=$(pwd -P)
+
+### Get current task ID
+x=${SLURM_ARRAY_TASK_ID}
+
+### The task is going to run a series of 4 jobs. For each job:
+for iter in {0..3}; do
+
+  ### Select a cluster Id
+  xi=$((4*x+iter))
+
+  ### Add zeros to the cluster Id
+  if [ $x -lt 10 ]; then
+      xstr="000${xi}"
+  elif [ $x -lt 100 ]; then
+      xstr="00${xi}"
+  elif [ $x -lt 1000 ]; then
+      xstr="0${xi}"
+  else
+      xstr="${xi}"
+  fi
+
+   ### Run GEOS-Chem in the directory corresponding to the cluster Id
+  cd  ${RUNDIR}/{RunName}_${xstr}
+  ./{RunName}_${xstr}.run
+
+done
+
+exit 0
